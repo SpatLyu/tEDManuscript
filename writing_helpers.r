85644b41543a08d53ca10860e47f590b70f27096
SpatLyu <lyu.geosocial@gmail.com>
2026-01-13 00:06:00 +0800

    include revised figures of png format

--- writing_helpers.r
+++ writing_helpers.r
@@ -3,10 +3,11 @@
   pdftools::pdf_convert(pdf = pdfname, filenames = pngname, dpi = dpi)
 }
 
-1:6 |>
+fs::dir_ls("./manuscript_r1/figure_pdf/", regexp = ".pdf$") |>
+  stringr::str_sub(28,-5) |> 
   purrr::walk(\(.x) {
-    .pdf2png(paste0('./manuscript_origin/figure_pdf/figure',.x,'.pdf'),
-             paste0('./manuscript_origin/figure/figure',.x,'.png'))
+    .pdf2png(paste0('./manuscript_r1/figure_pdf/',.x,'.pdf'),
+             paste0('./manuscript_r1/figure/',.x,'.png'))
   })
 
 # references: doi -> bibtex
